b19e35fbde381e0cd1e7bc8c7901a690172362fb
namtalb <jsblatman@gmail.com>
2017-11-08 17:12:50 -0800

    first 5 questions completed

--- app.js
+++ app.js
@@ -1,49 +1,64 @@
 'use strict';
 
-var friend = prompt('Am I your friend?');
-console.log('am I your friend?:', friend);
-friend = friend.toLowerCase();
+var developer;
+var prospectiveJobSeeker;
+var want;
+var hats;
+var Ohio;
+var favnum;
 
-if ((friend === 'y') || (friend === 'yes')){
-  alert('Rock on Yo!');
+var developer = prompt('Am I a developer?').toLowerCase();
+console.log('Am I a developer?:', developer);
+
+if ((developer === 'y') || (developer === 'yes')){
+  alert('Nice work! Maybe you can guess a few more fun facts about me!');
+} else {
+  if ((developer === 'n') || (developer === 'no')){
+    alert('Incorrect! Try again!');
+  }
 }
 
-var houseIsland = prompt('Is my house on an island?');
-console.log('house on an island:', houseIsland);
-houseIsland = houseIsland.toLowerCase();
+var prospectiveJobSeeker = prompt('Am I looking for a job?').toLowerCase();
+console.log('Am I looking for a job?:', prospectiveJobSeeker);
 
-if ((friend === 'y' || houseIsland  === 'n') || (friend === 'yes' || houseIsland === 'no')) {
-  alert('You know so much!');
+if ((prospectiveJobSeeker === 'y') || (prospectiveJobSeeker = 'yes')){
+alert('wow, how\'d you know?!');
+} else {
+  if ((prospectiveJobSeeker === 'n') || (prospectiveJobSeeker = 'no')){
+    alert('wrong answer! ... but not exactly wrong, persay...');
+  }
 }
 
-var seeingPlay = prompt('Are we seeing a play tonight?');
-console.log('seeing a play:', seeingPlay);
-seeingPlay = seeingPlay.toLowerCase();
-
-if ((friend === 'y' || seeingPlay === 'y') || (friend === 'yes' || seeingPlay === 'yes')) {
-  alert('well, what if we saw a good one?');
+var want = prompt('Do I know what I want?').toLowerCase();
+if ((want === 'y') || (want = 'yes')){
+alert('wow, you\'re good!! And thanks for your trust! For now, let\'s change the topic...');
+} else {
+  if ((want === 'n') || (want = 'no')){
+    alert('duh, I want to change the world, fool! For now, let\'s change the topic...');
+  } else {
+    alert('look \'man\', this isn\'t complicated- it\'s a yes\/no question. Let\'s change the topic...')};
 }
 
-if ((friend === 'n' || houseIsland === 'n') || (friend === 'no' || houseIsland === 'no')) {
-  alert('the sun will come out... tomorrow...');
+var hats = prompt('Do you know how many hats I own?').toLowerCase();
+if ((hats === 'y') || (hats = 'yes')){
+alert('ok, mr/mrs smarty pants, how many?');
+} else {
+  if ((hats === 'n') || (hats = 'no')){
+    alert('That\'s fair, I don\'t know how many hats you own either!')
+  } else {
+    alert('maybe that IS a silly question!')
+  }
 }
 
-var goldMined = prompt('is gold mined in Canada?');
-console.log('gold is mined:', goldMined);
-goldMined = goldMined.toLowerCase();
-
-var mayIHelp = prompt('May I help you?');
-console.log('May I help you:, mayIHelp');
-mayIHelp = mayIHelp.toLowerCase();
-
-if ((friend === 'n') || (friend === 'no')) {
-  alert('well, peanuts');
+var Ohio = prompt('Am I from Ohio?').toLowerCase();
+if ((Ohio === 'y') || (Ohio === 'yes')){
+  alert('This is great! You\'re really getting to know me pretty well, huh?');
+} else {
+  if ((Ohio === 'n') || (Ohio === 'no')){
+    alert('c\'mon, I pretty much handed that one to you...');
+  } else {
+    alert('omg... These have all been yes\/no questions so far!... let\'s totally change this up:');
+  }
 }
 
-if ((seeingPlay === 'y' || goldMined === 'n') || (seeingPlay === 'yes' || goldMined === 'no')) {
-alert('we have so much in common!');
-}
-
-if ((mayIHelp === 'n' && goldMined === 'y') || (mayIHelp === 'no' && goldMined === 'yes')) {
-  alert('then get after it!');
-}
+var favnum = prompt('What is my favorite number?');
